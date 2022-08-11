a11d89d7a936ef46a3bcd5c41400d2952cb49164
StasiSie <mail.koshaksa@gmail.com>
2022-08-11 16:43:47 +0300

    Initial Commit

---
+++ CombineUIApp/CombineUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineUIAppApp.swift
+//  CombineUIApp
+//
+//  Created by Игорь Богданов on 11.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
